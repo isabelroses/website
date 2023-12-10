9493ef4306242f38f2b4f867ba719eee6554fa6c
isabel <isabel@isabelroses.com>
2023-12-10 03:35:59 +0000

    chore: cleanup code

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,47 +1,54 @@
-import { createRouter, createWebHistory, NavigationFailure, RouteRecordRaw } from 'vue-router'
-import Home from '@/views/Home.vue'
+import {
+    createRouter,
+    createWebHistory,
+    NavigationFailure,
+    RouteRecordRaw,
+} from "vue-router";
+import Home from "@/views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
-        path: '/',
-        name: 'Home',
-        meta: { title: 'Home', nav: true },
-        component: Home
+        path: "/",
+        name: "Home",
+        meta: { title: "Home", nav: true },
+        component: Home,
     },
     {
-        path: '/projects',
-        name: 'Projects',
-        meta: { title: 'Projects', nav: true },
-        component: () => import('../views/Projects.vue')
+        path: "/projects",
+        name: "Projects",
+        meta: { title: "Projects", nav: true },
+        component: () => import("../views/Projects.vue"),
     },
     {
-        path: '/blog',
-        name: 'Blog',
-        meta: { title: 'Blog', nav: true },
-        component: () => import('../views/Blog.vue'),
-        props: route => (route.query)
+        path: "/blog",
+        name: "Blog",
+        meta: { title: "Blog", nav: true },
+        component: () => import("../views/Blog.vue"),
+        props: (route) => route.query,
     },
     {
-        path: '/blog/:slug',
-        name: 'BlogPost',
-        meta: { title: 'Blog', nav: true },
-        component: () => import('../views/BlogPost.vue'),
+        path: "/blog/:slug",
+        name: "BlogPost",
+        meta: { title: "Blog", nav: true },
+        component: () => import("../views/BlogPost.vue"),
     },
-]
+];
 
 export const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
-export function pushQuery(query: { [id: string]: string | null }): Promise<void | NavigationFailure | undefined> {
-    const queries = { ...router.currentRoute.value.query ?? {} }
+export function pushQuery(query: {
+    [id: string]: string | null;
+}): Promise<void | NavigationFailure | undefined> {
+    const queries = { ...(router.currentRoute.value.query ?? {}) };
 
     for (const k of Object.keys(query)) {
-        if (query[k] == null) delete queries[k]
-        else queries[k] = query[k]
+        if (query[k] == null) delete queries[k];
+        else queries[k] = query[k];
     }
 
-    return router.push({ query: queries })
+    return router.push({ query: queries });
 }
-export default router
+export default router;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import {Vue} from "vue-class-component";
+import { Vue } from "vue-class-component";
 
 /**
  * Same as python's range
@@ -12,15 +12,16 @@ import {Vue} from "vue-class-component";
  * @param to to, Not inclusive
  * @param step Step
  */
-export function range(fromOrTo: number, to?: number, step = 1): number[]
-{
-    const from = to !== undefined ? fromOrTo : 0
-    to = to !== undefined ? to : fromOrTo
+export function range(fromOrTo: number, to?: number, step = 1): number[] {
+    const from = to ?? 0;
+    to = to ?? fromOrTo;
 
-    if (to == from) return []
-    const mul = to > from ? 1 : -1
+    if (to == from) return [];
+    const mul = to > from ? 1 : -1;
 
-    return [...Array(Math.floor((Math.abs(to - from)) / step))].map((_, i) => from + i * step * mul);
+    return [...Array(Math.floor(Math.abs(to - from) / step))].map(
+        (_, i) => from + i * step * mul
+    );
 }
 
 /**
@@ -30,79 +31,83 @@ export function range(fromOrTo: number, to?: number, step = 1): number[]
  * @param min
  * @param max
  */
-export function minMax(val: number, min: number, max: number): number
-{
-    return val > max ? max : val < min ? min: val
+export function minMax(val: number, min: number, max: number): number {
+    let result: number;
+
+    if (val > max) {
+        result = max;
+    } else if (val < min) {
+        result = min;
+    } else {
+        result = val;
+    }
+
+    return result;
 }
 
 /**
  * Keybinds[key string] = (event) => Prevent default or not (Default: true)
  */
-export type Keybinds = {[id: string]: (e: KeyboardEvent) => unknown}
+export type Keybinds = { [id: string]: (e: KeyboardEvent) => unknown };
 
 /**
  * Key handler mixin
  */
-export class KeyHandler extends Vue
-{
-    keybinds: Keybinds = {}
-    _keybinds: Keybinds = {}
+export class KeyHandler extends Vue {
+    keybinds: Keybinds = {};
+    _keybinds: Keybinds = {};
 
-    initKeybinds(): void
-    {
-        return
+    initKeybinds(): void {
+        return;
     }
 
-    mounted(): void
-    {
-        document.addEventListener('keydown', this.keyListener)
-        this.initKeybinds()
-        Object.keys(this.keybinds).forEach(it => this._keybinds[it.toLowerCase()] = this.keybinds[it])
+    mounted(): void {
+        document.addEventListener("keydown", this.keyListener);
+        this.initKeybinds();
+        Object.keys(this.keybinds).forEach(
+            (it) => (this._keybinds[it.toLowerCase()] = this.keybinds[it])
+        );
     }
 
-    unmounted(): void
-    {
-        document.removeEventListener('keydown', this.keyListener)
+    unmounted(): void {
+        document.removeEventListener("keydown", this.keyListener);
     }
 
-    keyListener(e: KeyboardEvent): void
-    {
-        let key = e.key
-        if (e.shiftKey) key = 'Shift' + key
-        if (e.altKey) key = 'Alt' + key
-        if (e.ctrlKey) key = 'Ctrl' + key
-        if (e.metaKey) key = 'Cmd' + key
-        key = key.toLowerCase()
+    keyListener(e: KeyboardEvent): void {
+        let key = e.key;
+        if (e.shiftKey) key = "Shift" + key;
+        if (e.altKey) key = "Alt" + key;
+        if (e.ctrlKey) key = "Ctrl" + key;
+        if (e.metaKey) key = "Cmd" + key;
+        key = key.toLowerCase();
 
-
-        if (key in this._keybinds)
-        {
-            if (this._keybinds[key](e) !== false)
-            {
-                e.preventDefault()
+        if (key in this._keybinds) {
+            if (this._keybinds[key](e) !== false) {
+                e.preventDefault();
             }
         }
     }
 }
 
-export function capitalize(s: string): string
-{
+export function capitalize(s: string): string {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
-export function shuffle(array: any[])
-{
-    let currentIndex = array.length, randomIndex;
+export function shuffle(array: any[]) {
+    let currentIndex = array.length,
+        randomIndex;
 
     // While there remain elements to shuffle.
-    while (currentIndex != 0)
-    {
+    while (currentIndex != 0) {
         // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
         // And swap it with the current element.
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex],
+            array[currentIndex],
+        ];
     }
 
     return array;
