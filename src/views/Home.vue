b61b8a6b60c896c6de66325b3dfe0c0736660188
isabel <isabel@isabelroses.com>
2024-01-08 12:44:19 +0000

    chore: cleanup homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="markdown-content general-page">
-    <Dynamic :template="html"></Dynamic>
+    <Dynamic :template="content" v-if="content" />
+    <Loading v-else />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import { parseMarkdown } from "@/lib/parseMarkdown";
-import { hosts } from "@/lib/constants";
 import Loading from "@/components/Loading.vue";
 import me from "@/gen/me.json";
 
 @Options({ components: { Loading } })
 export default class Home extends Vue {
-  html = "";
+  content = "";
 
-  mounted(): void {
-    this.html = parseMarkdown(me.content);
+  async mounted(): void {
+    if (me.content) {
+      this.content = await parseMarkdown(me.content);
+    }
   }
 }
 </script>
