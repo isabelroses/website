ccf52bde785ba62bdb2f6488da2370b6ae557e96
isabel <isabel@isabelroses.com>
2024-01-09 02:17:11 +0000

    feat: css gen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.{html,go}"],
   safelist: [{
     pattern: /hljs+/,
   }],
+  darkMode: "class",
   theme: {
-    hljs: {
-      theme: "../../../ctp-highlightjs-theme", // this is so cursed
+    colors: {
+      "fg": "var(--fg)",
+      "fg-lighter": "var(--fg-lighter)",
+      "bg": "var(--bg)",
+      "bg-lighter": "var(--bg-lighter)",
+      "bg-darker": "var(--bg-darker)",
+      "card": "var(--card)",
+      "card-lighter": "var(--card-lighter)",
+      "special": "var(--special)",
     },
+    fontFamily: {
+			sans: ['"Roboto"', ...defaultTheme.fontFamily.sans],
+      mono: ['"Roboto Mono"', ...defaultTheme.fontFamily.mono],
+		},
   },
   plugins: [
-    require("@catppuccin/tailwindcss"),
     require("@tailwindcss/typography"),
     require("tailwind-highlightjs"),
   ],
