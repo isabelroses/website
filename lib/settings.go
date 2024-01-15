ec7ee2c297fd63ec34273883ae9625066dc979b0
isabel <isabel@isabelroses.com>
2024-01-15 02:41:37 +0000

    chore: tpyo

--- lib/settings.go
+++ lib/settings.go
@@ -1,7 +1,7 @@
 package lib
 
 // Default settings
-var RootDir string = "./" // remeber trailing slash
+var RootDir string = "./" // remember trailing slash
 
 func GetPath(path string) string {
 	return RootDir + path
