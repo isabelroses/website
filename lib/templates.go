53da4ee365579f8ce51590263dca012036e7395c
isabel <isabel@isabelroses.com>
2024-01-26 20:18:32 +0000

    feat(lib): create a template generator

---
+++ lib/templates.go
@@ -0,0 +1,38 @@
+package lib
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"runtime"
+)
+
+func RenderTemplate(w http.ResponseWriter, layout string, components []string, props interface{}) error {
+	_, filename, _, _ := runtime.Caller(1)
+	page := filepath.Base(filename)
+	page = page[:len(page)-len(filepath.Ext(page))]
+
+	templates := []string{
+		GetPath("/templates/layouts/" + layout + ".html"),
+		GetPath("/templates/pages/" + page + ".html"),
+	}
+
+	for _, component := range components {
+		templates = append(templates, GetPath("/templates/components/" + component + ".html"))
+	}
+
+	ts, err := template.ParseFiles(templates...)
+	if err != nil {
+		log.Print(err.Error())
+		return err
+	}
+
+	err = ts.ExecuteTemplate(w, layout, props)
+	if err != nil {
+		log.Print(err.Error())
+		return err
+	}
+
+	return nil
+}
