2f0a479ec3416408f7e51c8f548202af5753f2bf
isabel <isabel@isabelroses.com>
2024-06-21 22:05:35 +0100

    refactor: cleanup styling

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -4,7 +4,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["../templates/**/*.tmpl", "../lib/codeparser.go"],
   safelist: [
-    { pattern: /.*hljs+/ },
     { pattern: /theme+/ },
   ],
   darkMode: "class",
